b9b062f1372479e3e20cc75545f78a32121fb9ad
Ellian Abe <ellian_abe@outlook.com>
2020-12-01 17:18:02 -0300

    + fluxo nos comentários

--- app/src/main/java/com/uniso/lpdm/horascomplementares/LoginActivity.java
+++ app/src/main/java/com/uniso/lpdm/horascomplementares/LoginActivity.java
@@ -21,9 +21,27 @@ public class LoginActivity extends AppCompatActivity {
         PARA IR PARA O DASHBOARD DO ALUNO
         Usuário: aluno
         Senha: aluno
+
         PARA IR PARA O DASHBOARD DO COORDENADOR
         Usuário: coordenador
         Senha: coordenador
+
+        fluxo da aplicação: ao realizar o login como aluno você é direcionado para o dashboard principal
+        nele há uma visualização da contagem das horas de atividades que estão com o status 10 (aprovada) no banco
+        sqlite. Há uma opção para compartilhar o progresso como um texto formatado e uma opção de cadastro
+        de novas atividades, que abrira uma tela onde é possível selecionar uma descrição, uma categoria e
+        a quantidade de horas da atividade a ser lançada.
+
+        Criada a atividade, não há impacto no dashboard do aluno, uma vez que o status inicial
+        da atividade é 0 (pendente), para que o status da atividade seja alterado, é preciso que o coordenador
+        faça a aprovação ou rejeição.
+        Ao realizar o login como coordenador, é possível visualizar uma lista com todos as atividades que estão
+        com o status 0 (pendente) e para aprovar ou rejeitar uma atividade o coordenador deve selecionar um grupo
+        de uma ou mais atividades (pressionando as atividades para serem adicionadas ou removidas da seleção)
+        *** atividades selecionadas -> background acizentado.
+        Clicando em aprovar ou rejeitar, o status das atividades selecionadas será alterado, fazendo com que
+        deixem de aparecer no dashboard do coordenador. Caso aprovadas, será alterado o saldo de horas no
+        dashboard do aluno (desde que não haja excesso baseado no limite de cada categoria de atividade)
      */
 
     @Override
